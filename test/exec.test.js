e2305f246ffe982bd5b0e5f386a69bd3e07bb9c5
Konstantin Käfer <github@kkaefer.com>
2011-02-23 17:38:40 -0500

    more elaborate test for Database#exec

--- test/exec.test.js
+++ test/exec.test.js
@@ -10,14 +10,25 @@ exports['test Database#exec'] = function(beforeExit) {
     db.exec(sql, function(err) {
         if (err) throw err;
 
-        db.all("SELECT name FROM sqlite_master WHERE type = 'table' ORDER BY name", function(err, rows) {
+        db.all("SELECT type, name FROM sqlite_master ORDER BY type, name", function(err, rows) {
             if (err) throw err;
 
-            assert.ok(rows.length, 3);
             assert.deepEqual(rows, [
-                { name: 'bar' },
-                { name: 'baz' },
-                { name: 'foo' }
+                { type: 'index', name: 'grid_key_lookup' },
+                { type: 'index', name: 'grid_utfgrid_lookup' },
+                { type: 'index', name: 'images_id' },
+                { type: 'index', name: 'keymap_lookup' },
+                { type: 'index', name: 'map_index' },
+                { type: 'index', name: 'name' },
+                { type: 'table', name: 'grid_key' },
+                { type: 'table', name: 'grid_utfgrid' },
+                { type: 'table', name: 'images' },
+                { type: 'table', name: 'keymap' },
+                { type: 'table', name: 'map' },
+                { type: 'table', name: 'metadata' },
+                { type: 'view', name: 'grid_data' },
+                { type: 'view', name: 'grids' },
+                { type: 'view', name: 'tiles' }
             ]);
 
             finished = true;
