6a7f5e590fa45d221d345d24b8507c0f5af26eb5
Dane Springmeyer <dane@dbsgeo.com>
2011-08-27 11:30:01 -0700

    only test loading spatialite extension if it actually exists

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,16 +1,23 @@
 var sqlite3 = require('sqlite3');
 var assert = require('assert');
+var path = require('path');
 
 if (process.setMaxListeners) process.setMaxListeners(0);
+
+var spatialite_ext = '/usr/local/lib/libspatialite.dylib';
 
 exports['test loadExtension'] = function(beforeExit) {
     var db = new sqlite3.Database(':memory:');
     var completed = false;
 
-    db.loadExtension('/usr/local/lib/libspatialite.dylib', function(err) {
-        if (err) throw err;
+    if (path.existsSync(spatialite_ext)) {
+        db.loadExtension(spatialite_ext, function(err) {
+            if (err) throw err;
+            completed = true;
+        });
+    } else {
         completed = true;
-    });
+    }
 
     beforeExit(function() {
         assert.ok(completed);
