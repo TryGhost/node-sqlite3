501caf03a215e4fb9429351d37165330041e0815
Dane Springmeyer <dane@dbsgeo.com>
2011-11-06 16:17:11 -0800

    Revert change to affected.test.js

--- test/affected.test.js
+++ test/affected.test.js
@@ -9,24 +9,22 @@ exports['test row changes and lastID'] = function(beforeExit) {
     var finished = false;
 
     db.serialize(function() {
-        db.run("CREATE TABLE  foo (id INT, txt TEXT)", function(err) {
-            if (err) throw err;
-            var stmt = db.prepare("INSERT INTO foo VALUES(?, ?)");
-            var j = 1;
-            for (var i = 0; i < 1000; i++) {
-                stmt.run(i, "demo", function(err) {
-                    if (err) throw err;
-                    // Relies on SQLite's row numbering to be gapless and starting
-                    // from 1.
-                    assert.equal(j++, this.lastID);
-                });
-            }
-    
-            db.run("UPDATE foo SET id = id + 1 WHERE id % 2 = 0", function(err) {
+        db.run("CREATE TABLE foo (id INT, txt TEXT)");
+        var stmt = db.prepare("INSERT INTO foo VALUES(?, ?)");
+        var j = 1;
+        for (var i = 0; i < 1000; i++) {
+            stmt.run(i, "demo", function(err) {
                 if (err) throw err;
-                assert.equal(500, this.changes);
-                finished = true;
+                // Relies on SQLite's row numbering to be gapless and starting
+                // from 1.
+                assert.equal(j++, this.lastID);
             });
+        }
+
+        db.run("UPDATE foo SET id = id + 1 WHERE id % 2 = 0", function(err) {
+            if (err) throw err;
+            assert.equal(500, this.changes);
+            finished = true;
         });
     });
 
