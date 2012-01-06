e6b1f79f7cdb83535654b38104a603d40f210850
Dane Springmeyer <dane@dbsgeo.com>
2012-01-06 15:38:52 -0800

    add gyp files for building without messing with visual studio UI

---
+++ vcbuild.bat
@@ -0,0 +1,8 @@
+del build.sln
+python gyp/gyp build.gyp --depth=. -f msvs -G msvs_version=2010
+msbuild build.sln
+copy Default\\node_sqlite3.node lib\\node_sqlite3.node
+rem test!
+rem set NODE_PATH=lib
+rem node node_modules\expresso\bin\expresso
+rem node -e "console.log(require('eio'))"
