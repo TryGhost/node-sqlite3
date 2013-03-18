26e7c65f84e1c8c97cd5df35fec96ddafa59175a
Dane Springmeyer <dane@dbsgeo.com>
2013-03-18 13:24:52 -0700

    fix the fts test added in 42aa8c887868916e1406cae4e3d9a1c1055c705c, which was not valid for expresso and must never have worked (just hung)

--- test/fts-content.test.js
+++ test/fts-content.test.js
@@ -1,17 +1,9 @@
-/*jshint strict:true node:true es5:true onevar:true laxcomma:true laxbreak:true eqeqeq:true immed:true latedef:true*/
-(function () {
-  "use strict";
+var sqlite = require('sqlite3')
+var assert = require('assert')
 
-  var sqlite = require('sqlite3')
-    , assert = require('assert')
-    , db
-    ;
-
-  function readyForQuery() {
+exports['test fts content'] = function(beforeExit) {
+    var db = new sqlite.Database(":memory:");
     db.exec('CREATE VIRTUAL TABLE t1 USING fts4(content="", a, b, c);', function (err) {
       assert.ok(!err);
     });
-  }
-
-  db = new sqlite.Database(":memory:", readyForQuery);
-}());
+}
