91f805c9e3629bfb902211245b94626395ec078f
Dane Springmeyer <dane@mapbox.com>
2014-06-28 22:06:22 -0700

    fix typo

--- scripts/build_against_node_webkit.sh
+++ scripts/build_against_node_webkit.sh
@@ -26,7 +26,7 @@ if [[ $(uname -s) == 'Darwin' ]]; then
     export NW_DOWNLOAD=node-webkit-v${NODE_WEBKIT}-osx-ia32
     wget http://dl.node-webkit.org/v${NODE_WEBKIT}/${NW_DOWNLOAD}.zip
     unzip -q ${NW_DOWNLOAD}.zip
-    export PATH=$(pwd)/node-webkit.app/Contents/MacOS/
+    export PATH=$(pwd)/node-webkit.app/Contents/MacOS/:${PATH}
 else
     export NW_DOWNLOAD=node-webkit-v${NODE_WEBKIT}-linux-x64
     # for testing node-webkit, launch a virtual display
@@ -34,7 +34,7 @@ else
     sh -e /etc/init.d/xvfb start +extension RANDR
     wget http://dl.node-webkit.org/v${NODE_WEBKIT}/${NW_DOWNLOAD}.tar.gz
     tar xf ${NW_DOWNLOAD}.tar.gz
-    export PATH=$(pwd)/${NW_DOWNLOAD}:$PATH
+    export PATH=$(pwd)/${NW_DOWNLOAD}:${PATH}
 fi
 
 npm install --build-from-source ${GYP_ARGS}
