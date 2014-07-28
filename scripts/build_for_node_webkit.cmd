8090219d44a94888a436b0b85d0115e228396fa0
Mithgol <getgit@mithgol.ru>
2014-07-28 22:49:14 +0400

    [skip ci] a command file that would build sqlite3 for node-webkit

---
+++ scripts/build_for_node_webkit.cmd
@@ -0,0 +1,9 @@
+node-pre-gyp clean
+npm install nw-gyp
+cinst wget 7zip.commandline
+wget http://dl.node-webkit.org/v%nw_version%/node-webkit-v%nw_version%-win-ia32.zip
+7z e -onw node-webkit-v%nw_version%-win-ia32.zip
+dir nw
+set PATH=nw;%PATH%
+node-pre-gyp package testpackage --runtime=node-webkit --target=%nw_version% --target_arch=ia32
+if not "%CM%" == "%CM:[publish binary]=%" node-pre-gyp publish --msvs_version=2013 --runtime=node-webkit --target=%nw_version% --target_arch=ia32
