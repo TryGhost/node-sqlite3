13725b80d0db6a7e56078ddb3f0dc34d75c49ba8
Mithgol the Webmaster <getgit@mithgol.ru>
2014-07-29 12:08:57 +0400

    build, test, publish Linux 32-bit binaries for node-webkit

---
+++ scripts/build_linux32_node_webkit.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# rebuild node-sqlite3 for 32 bit node-webkit target
+node-pre-gyp rebuild ${GYP_ARGS}
+# install 32 bit stuff necessary for node-webkit 32 bit
+sudo apt-get -y install libx11-6:i386
+sudo apt-get -y install libxtst6:i386
+sudo apt-get -y install libcap2:i386
+sudo apt-get -y install libglib2.0-0:i386
+sudo apt-get -y install libgtk2.0-0:i386
+sudo apt-get -y install libatk1.0-0:i386
+sudo apt-get -y install libgdk-pixbuf2.0-0:i386
+sudo apt-get -y install libcairo2:i386
+sudo apt-get -y install libfreetype6:i386
+sudo apt-get -y install libfontconfig1:i386
+sudo apt-get -y install libxcomposite1:i386
+sudo apt-get -y install libasound2:i386
+sudo apt-get -y install libxdamage1:i386
+sudo apt-get -y install libxext6:i386
+sudo apt-get -y install libxfixes3:i386
+sudo apt-get -y install libnss3:i386
+sudo apt-get -y install libnspr4:i386
+sudo apt-get -y install libgconf-2-4:i386
+sudo apt-get -y install libexpat1:i386
+sudo apt-get -y install libdbus-1-3:i386
+sudo apt-get -y install libudev0:i386
+# download and unpack node-webkit, add it to PATH
+wget http://dl.node-webkit.org/v${NODE_WEBKIT}/node-webkit-v${NODE_WEBKIT}-linux-ia32.tar.gz
+tar xf node-webkit-v${NODE_WEBKIT}-linux-ia32.tar.gz
+OLD_PATH="$PATH";
+export PATH=$(pwd)/node-webkit-v${NODE_WEBKIT}-linux-ia32:$PATH
+# ldd nw (helps to find out if some necessary apt-get line is missing)
+ldd $(pwd)/node-webkit-v${NODE_WEBKIT}-linux-ia32/nw
+# attempt node-pre-gyp package testpackage
+node-pre-gyp package testpackage publish ${GYP_ARGS}
+# erase used node-webkit, restore PATH
+export PATH="$OLD_PATH"; rm -rf node-webkit-v${NODE_WEBKIT}-linux-ia32
