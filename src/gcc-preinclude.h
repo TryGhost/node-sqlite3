5efbf428092f7f0e068e3ecc2596c8f8c13ae04f
Dane Springmeyer <dane@mapbox.com>
2015-07-13 17:45:10 -0700

    fix compile for alpine linux / musl c library - closes #459

--- src/gcc-preinclude.h
+++ src/gcc-preinclude.h
@@ -1,6 +1,6 @@
 
 // https://rjpower9000.wordpress.com/2012/04/09/fun-with-shared-libraries-version-glibc_2-14-not-found/
 
-#if defined(__linux__) && defined(__x86_64__)
+#if defined(__linux__) && defined(__GLIBC__) && defined(__x86_64__)
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
 #endif
