aa1da32ba325934ea06789c8770c8fc547ca487b
Dane Springmeyer <dane@mapbox.com>
2015-11-02 14:04:16 -0800

    try 2

--- scripts/build_against_node.sh
+++ scripts/build_against_node.sh
@@ -14,8 +14,18 @@ function publish() {
 }
 
 # test installing from source
-npm install --build-from-source  --clang=1
-npm test
+if [[ ${COVERAGE} == true ]]; then
+    echo $CXX
+    echo $CXXFLAGS
+    echo $LDFLAGS
+    CXXFLAGS="--coverage" LDFLAGS="--coverage" npm install --build-from-source  --clang=1 --verbose
+    npm test
+    ./mason_packages/.link/bin/cpp-coveralls --exclude node_modules --exclude tests --build-root build --gcov-options '\-lp' --exclude docs --exclude build/Release/obj/gen --exclude deps
+else
+    npm install --build-from-source  --clang=1
+    npm test
+fi
+
 
 publish
 
