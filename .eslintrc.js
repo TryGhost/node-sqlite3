20337bc15cd7c57552893ee1cbf69a0632e00ae1
Tom MacWright <tom@macwright.org>
2016-09-23 11:48:21 -0400

    Add basic eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true
+    },
+    "rules": {
+        "indent": ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "semi": ["error", "always"],
+        "no-cond-assign": ["error", "always"]
+    }
+}
