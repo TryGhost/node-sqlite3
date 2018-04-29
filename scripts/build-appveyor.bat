59e209848a6103a922a800870d0f529f021e05fa
Kewde <kewde@particl.io>
2018-04-29 14:54:24 +0000

    trigger ci

--- scripts/build-appveyor.bat
+++ scripts/build-appveyor.bat
@@ -177,7 +177,7 @@ CALL npm test
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 :NPM_TEST_FINISHED
-
+ECHO packaging for node-gyp
 CALL node_modules\.bin\node-pre-gyp package %TOOLSET_ARGS%
 ::make commit message env var shorter
 SET CM=%APPVEYOR_REPO_COMMIT_MESSAGE%
