ed95fa591dfb3dcc78a15c3a321b1b31e5f46d3c
Richard Lindner <richard.lindner@sap.com>
2019-08-05 10:29:41 +0200

    Add a more involved test with multiple calls

--- test/user_functions.test.js
+++ test/user_functions.test.js
@@ -1,48 +1,50 @@
 var sqlite3 = require('..');
 var assert = require('assert');
 
-describe('user functions', function() {
+describe('user functions', function () {
     var db;
-    before(function(done) { db = new sqlite3.Database(':memory:', done); });
+    before(function (done) {
+        db = new sqlite3.Database(':memory:', done);
+    });
 
-    it('should allow registration of user functions', function() {
-        db.registerFunction('MY_UPPERCASE', function(value) {
+    it('should allow registration of user functions', function () {
+        db.registerFunction('MY_UPPERCASE', function (value) {
             return value.toUpperCase();
         });
-        db.registerFunction('MY_STRING_JOIN', function(value1, value2) {
+        db.registerFunction('MY_STRING_JOIN', function (value1, value2) {
             return [value1, value2].join(' ');
         });
-        db.registerFunction('MY_Add', function(value1, value2) {
+        db.registerFunction('MY_Add', function (value1, value2) {
             return value1 + value2;
         });
-        db.registerFunction('MY_REGEX', function(regex, value) {
+        db.registerFunction('MY_REGEX', function (regex, value) {
             return !!value.match(new RegExp(regex));
         });
-        db.registerFunction('MY_REGEX_VALUE', function(regex, value) {
+        db.registerFunction('MY_REGEX_VALUE', function (regex, value) {
             return /match things/i;
         });
-        db.registerFunction('MY_ERROR', function(value) {
+        db.registerFunction('MY_ERROR', function (value) {
             throw new Error('This function always throws');
         });
-        db.registerFunction('MY_UNHANDLED_TYPE', function(value) {
+        db.registerFunction('MY_UNHANDLED_TYPE', function (value) {
             return {};
         });
-        db.registerFunction('MY_NOTHING', function(value) {
+        db.registerFunction('MY_NOTHING', function (value) {
 
         });
     });
 
-    it('should process user functions with one arg', function(done) {
-        db.all('SELECT MY_UPPERCASE("hello") AS txt', function(err, rows) {
+    it('should process user functions with one arg', function (done) {
+        db.all('SELECT MY_UPPERCASE("hello") AS txt', function (err, rows) {
             if (err) throw err;
             assert.equal(rows.length, 1);
-            assert.equal(rows[0].txt, 'HELLO')
+            assert.equal(rows[0].txt, 'HELLO');
             done();
         });
     });
 
-    it('should process user functions with two args', function(done) {
-        db.all('SELECT MY_STRING_JOIN("hello", "world") AS val', function(err, rows) {
+    it('should process user functions with two args', function (done) {
+        db.all('SELECT MY_STRING_JOIN("hello", "world") AS val', function (err, rows) {
             if (err) throw err;
             assert.equal(rows.length, 1);
             assert.equal(rows[0].val, 'hello world');
@@ -50,8 +52,8 @@ describe('user functions', function() {
         });
     });
 
-    it('should process user functions with number args', function(done) {
-        db.all('SELECT MY_ADD(1, 2) AS val', function(err, rows) {
+    it('should process user functions with number args', function (done) {
+        db.all('SELECT MY_ADD(1, 2) AS val', function (err, rows) {
             if (err) throw err;
             assert.equal(rows.length, 1);
             assert.equal(rows[0].val, 3);
@@ -59,8 +61,8 @@ describe('user functions', function() {
         });
     });
 
-    it('allows writing of a regex function', function(done) {
-        db.all('SELECT MY_REGEX("colou?r", "color") AS val', function(err, rows) {
+    it('allows writing of a regex function', function (done) {
+        db.all('SELECT MY_REGEX("colou?r", "color") AS val', function (err, rows) {
             if (err) throw err;
             assert.equal(rows.length, 1);
             assert.equal(Boolean(rows[0].val), true);
@@ -68,8 +70,8 @@ describe('user functions', function() {
         });
     });
 
-    it('converts returned regex instances to strings', function(done) {
-        db.all('SELECT MY_REGEX_VALUE() AS val', function(err, rows) {
+    it('converts returned regex instances to strings', function (done) {
+        db.all('SELECT MY_REGEX_VALUE() AS val', function (err, rows) {
             if (err) throw err;
             assert.equal(rows.length, 1);
             assert.equal(rows[0].val, '/match things/i');
@@ -77,16 +79,16 @@ describe('user functions', function() {
         });
     });
 
-    it('reports errors thrown in functions', function(done) {
-        db.all('SELECT MY_ERROR() AS val', function(err, rows) {
+    it('reports errors thrown in functions', function (done) {
+        db.all('SELECT MY_ERROR() AS val', function (err, rows) {
             assert.equal(err.message, 'SQLITE_ERROR: Uncaught Error: This function always throws');
             assert.equal(rows, undefined);
             done();
         });
     });
 
-    it('reports errors for unhandled types', function(done) {
-        db.all('SELECT MY_UNHANDLED_TYPE() AS val', function(err, rows) {
+    it('reports errors for unhandled types', function (done) {
+        db.all('SELECT MY_UNHANDLED_TYPE() AS val', function (err, rows) {
             assert.equal(err.message, 'SQLITE_ERROR: invalid return type in ' +
                 'user function MY_UNHANDLED_TYPE');
             assert.equal(rows, undefined);
@@ -94,8 +96,8 @@ describe('user functions', function() {
         });
     });
 
-    it('allows no return value from functions', function(done) {
-        db.all('SELECT MY_NOTHING() AS val', function(err, rows) {
+    it('allows no return value from functions', function (done) {
+        db.all('SELECT MY_NOTHING() AS val', function (err, rows) {
             if (err) throw err;
             assert.equal(rows.length, 1);
             assert.equal(rows[0].val, undefined);
@@ -103,5 +105,44 @@ describe('user functions', function() {
         });
     });
 
-    after(function(done) { db.close(done); });
+    it('make multiple calls to function', function (done) {
+        var count = 100;
+        var called = 0;
+        var sum = 0;
+
+        db.registerFunction('MY_SQUARE', function (value1) {
+            called++;
+            return value1 * value1;
+        });
+
+        db.serialize(function () {
+            db.run("CREATE TABLE foo (id INT)");
+
+            var stmt = db.prepare("INSERT INTO foo (id) VALUES (?)");
+            for (var i = 1; i <= count; i++) {
+                stmt.run(i, function (err) {
+                    if (err) throw err;
+                });
+            }
+            stmt.finalize();
+
+            db.all("SELECT MY_SQUARE(id) AS square FROM foo", function (err, rows) {
+                if (err) throw err;
+                rows.forEach(function (row) {
+                    sum += row.square;
+                });
+            });
+
+            db.run("DROP TABLE foo");
+        });
+        db.wait(function () {
+            assert.equal(called, count);
+            assert.equal(sum, count * (count + 1) * (2 * count + 1) / 6);
+            done();
+        });
+    });
+
+    after(function (done) {
+        db.close(done);
+    });
 });
