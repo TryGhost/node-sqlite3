a779446088df8ea44ac1fe2fb4ec02b692c66a02
Daniel Lockyer <hi@daniellockyer.com>
2022-04-21 16:32:32 +0100

    Moved glibc check inside Linux check
    refs https://github.com/TryGhost/node-sqlite3/issues/1584
    
    - on freebsd, `features.h` is missing so the library will fail to compile
    - we actually only want to include these symbol overrides if we're on Linux,
      so this commit moves the checks around to tighten up the flow
    - a further iteration could remove the file from `bindings.gyp` if I can
      figure out how to only use it in CI

--- src/gcc-preinclude.h
+++ src/gcc-preinclude.h
@@ -1,11 +1,12 @@
-
 // http://web.archive.org/web/20140401031018/http://rjpower9000.wordpress.com:80/2012/04/09/fun-with-shared-libraries-version-glibc_2-14-not-found/
+
+#if defined(__linux__)
 
 #define _GNU_SOURCE
 #include <features.h>
 #undef _GNU_SOURCE
 
-#if defined(__linux__) && defined(__USE_GNU)
+#if defined(__USE_GNU)
 
 #if defined(__x86_64__)
 __asm__(".symver memcpy,memcpy@GLIBC_2.2.5");
@@ -23,4 +24,5 @@ __asm__(".symver pow,pow@GLIBC_2.17");
 __asm__(".symver fcntl64,fcntl@GLIBC_2.17");
 #endif
 
+#endif
 #endif
