9e9079d2d34ad112d5922057c1b3fe159357ad3c
Daniel Lockyer <hi@daniellockyer.com>
2022-09-14 09:13:50 +0100

    Updated types file
    - added constant exports and missing functions to Database

--- lib/sqlite3.d.ts
+++ lib/sqlite3.d.ts
@@ -1,8 +1,5 @@
-// Type definitions for sqlite3 3.1
-// Project: http://github.com/mapbox/node-sqlite3
-// Definitions by: Nick Malaguti <https://github.com/nmalaguti>
-//                 Sumant Manne <https://github.com/dpyro>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// Type definitions for sqlite3
+// Project: http://github.com/tryghost/node-sqlite3
 
 /// <reference types="node" />
 
@@ -11,9 +8,55 @@ import events = require("events");
 export const OPEN_READONLY: number;
 export const OPEN_READWRITE: number;
 export const OPEN_CREATE: number;
+export const OPEN_FULLMUTEX: number;
 export const OPEN_SHAREDCACHE: number;
 export const OPEN_PRIVATECACHE: number;
 export const OPEN_URI: number;
+
+export const VERSION: string;
+export const SOURCE_ID: string;
+export const VERSION_NUMBER: number;
+
+export const OK: number;
+export const ERROR: number;
+export const INTERNAL: number;
+export const PERM: number;
+export const ABORT: number;
+export const BUSY: number;
+export const LOCKED: number;
+export const NOMEM: number;
+export const READONLY: number;
+export const INTERRUPT: number
+export const IOERR: number;
+export const CORRUPT: number
+export const NOTFOUND: number;
+export const FULL: number;
+export const CANTOPEN: number;
+export const PROTOCOL: number;
+export const EMPTY: number;
+export const SCHEMA: number;
+export const TOOBIG: number
+export const CONSTRAINT: number
+export const MISMATCH: number;
+export const MISUSE: number;
+export const NOLFS: number;
+export const AUTH: number
+export const FORMAT: number;
+export const RANGE: number
+export const NOTADB: number;
+
+export const LIMIT_LENGTH: number;
+export const LIMIT_SQL_LENGTH: number;
+export const LIMIT_COLUMN: number;
+export const LIMIT_EXPR_DEPTH: number;
+export const LIMIT_COMPOUND_SELECT: number;
+export const LIMIT_VDBE_OP: number;
+export const LIMIT_FUNCTION_ARG: number;
+export const LIMIT_ATTACHED: number;
+export const LIMIT_LIKE_PATTERN_LENGTH: number;
+export const LIMIT_VARIABLE_NUMBER: number;
+export const LIMIT_TRIGGER_DEPTH: number;
+export const LIMIT_WORKER_THREADS: number;
 
 export const cached: {
     Database(filename: string, callback?: (this: Database, err: Error | null) => void): Database;
@@ -83,12 +126,17 @@ export class Database extends events.EventEmitter {
 
     on(event: "trace", listener: (sql: string) => void): this;
     on(event: "profile", listener: (sql: string, time: number) => void): this;
+    on(event: "change", listener: (type: string, database: string, table: string, rowid: number) => void): this;
     on(event: "error", listener: (err: Error) => void): this;
     on(event: "open" | "close", listener: () => void): this;
     on(event: string, listener: (...args: any[]) => void): this;
 
     configure(option: "busyTimeout", value: number): void;
     configure(option: "limit", id: number, value: number): void;
+
+    loadExtension(filename: string, callback?: (err: Error | null) => void): this;
+
+    wait(callback?: (param: null) => void): this;
 
     interrupt(): void;
 }
@@ -99,9 +147,56 @@ export interface sqlite3 {
     OPEN_READONLY: number;
     OPEN_READWRITE: number;
     OPEN_CREATE: number;
+    OPEN_FULLMUTEX: number;
     OPEN_SHAREDCACHE: number;
     OPEN_PRIVATECACHE: number;
     OPEN_URI: number;
+
+    VERSION: string;
+    SOURCE_ID: string;
+    VERSION_NUMBER: number;
+
+    OK: number;
+    ERROR: number;
+    INTERNAL: number;
+    PERM: number;
+    ABORT: number;
+    BUSY: number;
+    LOCKED: number;
+    NOMEM: number;
+    READONLY: number;
+    INTERRUPT: number
+    IOERR: number;
+    CORRUPT: number
+    NOTFOUND: number;
+    FULL: number;
+    CANTOPEN: number;
+    PROTOCOL: number;
+    EMPTY: number;
+    SCHEMA: number;
+    TOOBIG: number
+    CONSTRAINT: number
+    MISMATCH: number;
+    MISUSE: number;
+    NOLFS: number;
+    AUTH: number
+    FORMAT: number;
+    RANGE: number
+    NOTADB: number;
+
+    LIMIT_LENGTH: number;
+    LIMIT_SQL_LENGTH: number;
+    LIMIT_COLUMN: number;
+    LIMIT_EXPR_DEPTH: number;
+    LIMIT_COMPOUND_SELECT: number;
+    LIMIT_VDBE_OP: number;
+    LIMIT_FUNCTION_ARG: number;
+    LIMIT_ATTACHED: number;
+    LIMIT_LIKE_PATTERN_LENGTH: number;
+    LIMIT_VARIABLE_NUMBER: number;
+    LIMIT_TRIGGER_DEPTH: number;
+    LIMIT_WORKER_THREADS: number;
+
     cached: typeof cached;
     RunResult: RunResult;
     Statement: typeof Statement;
